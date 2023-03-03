37ae5b030a02a4d58b49d2a6ba611590d55b5aa5
Shri Ganesh Jha <realsgj@gmail.com>
2023-03-04 01:20:04 +0545

    Update api url

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/`, {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
